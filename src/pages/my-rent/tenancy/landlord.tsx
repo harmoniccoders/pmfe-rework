8d02570d9b0bc536afac8e7931aeb1dcb83e3af1
Gomina Maryam <gominamaryam@gmail.com>
2022-06-09 13:03:08 +0100

    fix tenant page

---
+++ src/pages/my-rent/tenancy/landlord.tsx
@@ -0,0 +1,45 @@
+import Landlord from 'lib/components/my-rent/Landlord';
+import { DataAccess } from 'lib/Utils/Api';
+import { returnUserData } from 'lib/Utils/userData';
+import { GetServerSideProps } from 'next';
+
+const index = ({ data }: { data: any }) => {
+  return <Landlord data={data} />;
+};
+
+export default index;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {
+    data: { redirect },
+  } = returnUserData(ctx);
+  if (redirect)
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+      props: {},
+    };
+
+  const bearer = `Bearer ${ctx.req.cookies.token}`;
+  const _dataAccess = new DataAccess(bearer);
+  let { url } = ctx.query;
+  url = 'limit=8&offset=0';
+  try {
+    const data = (await _dataAccess.get(`?${url}`)).data;
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        propertyTypes: {},
+        data: [],
+      },
+    };
+  }
+};
